de743f4d6561a09f4c760c6ce5619919a6fe98cd
Quds-Aljabri <qudsahmedaljabri@gmail.com>
2025-05-17 21:48:04 +0400

    Adding two numbers

---
+++ app.py
@@ -0,0 +1,18 @@
+num1 = 0 
+num2= 0
+operation = ""
+result = 0
+
+num1 = int(input("Please enter the 1st number: "))
+num2 = int(input("Please enter the 2nd number: "))
+operation = input("Please enter one of + - * or /: ")
+
+if operation == "+":
+    result = num1 + num2
+
+else:
+    print("[Warn]Operation not supported!")
+
+print("Result is: ",result, "!!!")
+
+print("End of script")
---
+++ calculator.py
@@ -0,0 +1,9 @@
+num1  = 0
+num2 = 0
+result = 0
+num1 = int(input("Enter the first num: "))
+num2 = int(input("Enter the first num: "))
+
+result = num1  + num2
+
+print(result)
